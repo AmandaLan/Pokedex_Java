11ee05d13cdec3bd057851fad9fb5e1210800e1c
Briacva <96231132+Briacva@users.noreply.github.com>
2022-04-05 16:37:08 +0200

    rooting +- fait

--- src/main/java/com/example/demo/controller/AccueilController.java
+++ src/main/java/com/example/demo/controller/AccueilController.java
@@ -1,14 +1,26 @@
 package com.example.demo.controller;
 
+import java.util.Collection;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.model.Pokemon;
+import com.example.demo.repository.PokemonRepository;
 
 @Controller
 public class AccueilController {
+	
+	@Autowired
+	private PokemonRepository pokemonRepository;
+	
 	@RequestMapping("/")
 	public String intro()
 	{
@@ -27,4 +39,15 @@ public class AccueilController {
 		model.addAttribute("pokemon",pokemon);
 		return "addPokemon";
 	}
+	
+    @PostMapping("/addpokemonpage")
+    public String addPokemon(@Valid Pokemon pokemon, BindingResult result, Model model) { 
+    	
+        Iterable<Pokemon> allPokemons = pokemonRepository.findAll();
+        pokemon.setNumber(((Collection<?>) allPokemons).size()+1);
+        
+        pokemonRepository.save(pokemon);
+        return "redirect:/accueil";
+    }	
+	
 }
--- src/main/java/com/example/demo/controller/PokemonController.java
+++ src/main/java/com/example/demo/controller/PokemonController.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.model.Pokemon;
-import com.example.demo.repository.PokemonRepository;
 import com.example.demo.service.PokemonService;
 
 @CrossOrigin
@@ -17,18 +15,9 @@ import com.example.demo.service.PokemonService;
 public class PokemonController {
 	@Autowired
 	private PokemonService pokemonService;
-	
-	@Autowired
-	private PokemonRepository pokemonRepository;
 
 	@GetMapping("/pokemons")
 	public Iterable<Pokemon> getPokemons(){
 		return pokemonService.getPokemons();
 	}
-	
-//    @PostMapping("/addpokemon")
-//    public String addPokemon(@Valid Pokemon pokemon, BindingResult result, Model model) {        
-//        pokemonRepository.save(pokemon);
-//        return "redirect:/accueil";
-//    }	
 }
